7dfeb35469248ed4004d2f1d37343f17472f6e66
Subhi2911 <msubhasmita29@gmail.com>
2025-07-01 14:38:05 +0530

    adding frontend url

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ connectToMongo();
 const app = express()
 const port = process.env.PORT || 5000
 
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:3000', 'https://inotebook-save-your-notes.onrender.com'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
 
 app.use(express.json());
 
