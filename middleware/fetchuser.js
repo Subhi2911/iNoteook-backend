c9eb53184bf3645351579fe79c611d22ca12f599
Subhi2911 <msubhasmita29@gmail.com>
2025-07-01 17:24:27 +0530

    JWT secret invalid

--- middleware/fetchuser.js
+++ middleware/fetchuser.js
@@ -1,14 +1,19 @@
 var jwt = require('jsonwebtoken');
-const JWT_SECRET = 'Baibhavisd$umbo';
+require('dotenv').config({ path: '.env.local' });
+const JWT_SECRET = process.env.JWT_SECRET;
+console.log("JWT_SECRET in fetchuser:", process.env.JWT_SECRET);
 
 const fetchuser = (req,res,next)=>{
+    
     //get the user from the jwt token and add id to req object
     const token = req.header('auth-token')
     if(!token){
+        console.log('authToken:', token)
         return res.status(401).send({error: "Please authenticate using a valid token"})
     }
     try {
-       const data = jwt.verify(token ,JWT_SECRET)
+        console.log( process.env.JWT_SECRET);
+        const data = jwt.verify(token ,JWT_SECRET)
     req.user=data.user;
     next(); 
     } catch (error) {
